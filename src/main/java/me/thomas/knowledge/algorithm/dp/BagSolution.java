e0c8261f5753b563597881f5af377dceab6f6f2c
Thomas Zhao <zhao.xshg47@gmail.com>
2016-01-12 00:04:16 +0800

    modify comment

--- src/main/java/me/thomas/knowledge/algorithm/dp/BagSolution.java
+++ src/main/java/me/thomas/knowledge/algorithm/dp/BagSolution.java
@@ -1,10 +1,11 @@
 package me.thomas.knowledge.algorithm.dp;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
+ * 背包算法：动态规划
+ *
  * Created by thomas on 6/6/14.
  */
 class Bag {
@@ -14,7 +15,6 @@ class Bag {
     int capacity;
 
     /**
-     *
      * @param capacity 总容量
      */
     public Bag(int capacity) {
@@ -85,23 +85,23 @@ public class BagSolution {
 
     public static void main(String[] args) {
         Bag bag = new Bag(7);
-        List<Gemstone> list = Arrays.asList(
+        List<Gemstone> gemstones = Arrays.asList(
                 new Gemstone(20, 5),
                 new Gemstone(10, 4),
-                new Gemstone(10, 3)
+                new Gemstone(12, 3)
         );
 
         // 定义二维数组来存放所有的情况: 宝石从少到多，剩余容量从多到少。
-        int table[][] = new int[list.size() + 1][bag.getCapacity() + 1];
-        for (int i = 0, len = list.size(); i <= len; i++) { // i表示前i个宝石
+        int table[][] = new int[gemstones.size() + 1][bag.getCapacity() + 1];
+        for (int i = 0, len = gemstones.size(); i <= len; i++) { // i表示前i个宝石
             for (int j = bag.getCapacity(); j > 0; j--) { // j表示当前背包剩余容量
                 if (i == 0) {
                     table[i][j] = 0;
-                } else if (j >= list.get(i - 1).getWeight()) { // j>=list.get(i-1).getWeight()表示第i-1个宝石可以放入背包的剩余容量中
-                    if (table[i - 1][j - list.get(i - 1).getWeight()] + list.get(i - 1).getPrice() > table[i - 1][j]) {
-                        // 如果放入第i-1个宝石的总价值大于不放入时的价值。
-                        // 当然此时前i-1个宝石的剩余容量为j-list.get(i-1).getWeight()
-                        table[i][j] = table[i - 1][j - list.get(i - 1).getWeight()] + list.get(i - 1).getPrice();
+                } else if (j >= gemstones.get(i - 1).getWeight()) { // j>=gemstones.get(i-1).getWeight()表示第i个宝石可以放入背包的剩余容量中
+                    if (table[i - 1][j - gemstones.get(i - 1).getWeight()] + gemstones.get(i - 1).getPrice() > table[i - 1][j]) {
+                        // 如果放入第i个宝石的总价值大于不放入时的价值。
+                        // 当然此时前i-1个宝石的剩余容量为j-gemstones.get(i-1).getWeight()
+                        table[i][j] = table[i - 1][j - gemstones.get(i - 1).getWeight()] + gemstones.get(i - 1).getPrice();
                     } else {
                         table[i][j] = table[i - 1][j];
                     }
@@ -109,20 +109,20 @@ public class BagSolution {
             }
         }
 
-        // table[list.size()][bag.getCapacity()]就是计算出的最大价值
-        bag.setValue(table[list.size()][bag.getCapacity()]);
+        // table[gemstones.size()][bag.getCapacity()]就是计算出的最大价值
+        bag.setValue(table[gemstones.size()][bag.getCapacity()]);
 
         // 这里检查哪些宝石被放入了包中
         int cap = bag.getCapacity();
-        for (int i = list.size(); i > 0; i--) {
+        for (int i = gemstones.size(); i > 0; i--) {
             if (table[i][cap] > table[i - 1][cap]) {
-                list.get(i - 1).setFlag(1);
-                // 装入第i-1个宝石后背包能装入的体积就只剩下cap - list.get(i - 1).getWeight();
-                cap -= list.get(i - 1).getWeight();
+                gemstones.get(i - 1).setFlag(1);
+                // 装入第i-1个宝石后背包能装入的体积就只剩下cap - gemstones.get(i - 1).getWeight();
+                cap -= gemstones.get(i - 1).getWeight();
             }
         }
-        for (int i = 0; i < list.size(); i ++) {
-            System.out.print(list.get(i).getFlag() + "\t");
+        for (int i = 0; i < gemstones.size(); i ++) {
+            System.out.print(gemstones.get(i).getFlag() + "\t");
         }
         System.out.println(bag.getValue());
     }
