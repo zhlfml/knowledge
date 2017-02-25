3cbb9dcbd96f5d2ed5368c6c8352aaeeba38f334
Zhao Xinsheng <zhaoxinsheng@patsnap.com>
2017-02-25 10:39:55 +0800

    opt

--- src/main/java/me/thomas/knowledge/cache/redis/TenMinuteTutorial.java
+++ src/main/java/me/thomas/knowledge/cache/redis/TenMinuteTutorial.java
@@ -13,7 +13,8 @@ public class TenMinuteTutorial {
         jedis.append("name", " hello");
         System.out.println(jedis.get("name"));
 
-        jedis.set("counter", "0");
+        // 使用自增操作时，可以先不用先设置为0，因为当这个变量不存在时，会自动设置为0。
+//        jedis.set("counter", "0");
         jedis.incr("counter");
         System.out.println(jedis.get("counter"));
     }
