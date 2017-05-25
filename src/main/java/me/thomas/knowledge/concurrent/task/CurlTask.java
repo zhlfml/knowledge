845ba5c976d1a780e63ce9f5f076bb54b2c2bed5
Zhao Xinsheng <zhaoxinsheng@patsnap.com>
2017-05-25 21:03:31 +0800

    使用RestTemplate发送http请求。

--- src/main/java/me/thomas/knowledge/concurrent/task/CurlTask.java
+++ src/main/java/me/thomas/knowledge/concurrent/task/CurlTask.java
@@ -1,6 +1,15 @@
 package me.thomas.knowledge.concurrent.task;
 
-import me.thomas.knowledge.utils.HttpClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import me.thomas.knowledge.utils.UUIDUtil;
 
 /**
  * @author zhaoxinsheng
@@ -9,15 +18,62 @@ import me.thomas.knowledge.utils.HttpClient;
 public class CurlTask implements Task {
 
     private String url;
+    private RestTemplate restTemplate = new RestTemplate();
 
     public CurlTask(String url) {
         this.url = url;
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+        restTemplate.getMessageConverters().forEach(httpMessageConverter -> {
+            if (httpMessageConverter instanceof AbstractJackson2HttpMessageConverter) {
+                ((AbstractJackson2HttpMessageConverter) httpMessageConverter).setObjectMapper(objectMapper);
+            }
+        });
     }
 
     @Override
     public boolean execute() {
-        String result = HttpClient.get(url);
-        System.out.println(result);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Feedback> entity = new HttpEntity<>(new Feedback(UUIDUtil.genUUID(), UUIDUtil.genUUID() + "@patsnap.com", ""), headers);
+        String response = restTemplate.postForEntity(url, entity, String.class).getBody();
+        System.out.println(response);
         return true;
+    }
+
+    private static class Feedback {
+        private String userId;
+        private String email;
+        private String feedbackContent;
+
+        public Feedback(String userId, String email, String feedbackContent) {
+            this.userId = userId;
+            this.email = email;
+            this.feedbackContent = feedbackContent;
+        }
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getFeedbackContent() {
+            return feedbackContent;
+        }
+
+        public void setFeedbackContent(String feedbackContent) {
+            this.feedbackContent = feedbackContent;
+        }
     }
 }
--- src/test/java/me/thomas/knowledge/concurrent/CountDownLatchDemoTest.java
+++ src/test/java/me/thomas/knowledge/concurrent/CountDownLatchDemoTest.java
@@ -11,11 +11,11 @@ public class CountDownLatchDemoTest {
 
     private CountDownLatchDemo latchDemo = new CountDownLatchDemo();
     private OrderTask orderTask = new OrderTask();
-    private CurlTask curlTask = new CurlTask("http://127.0.0.1:3000/?text=%E6%88%91%E5%96%9C%E6%AC%A2%E8%BF%99%E9%97%A8%E6%8A%80%E6%9C%AF&to=de");
+    private CurlTask curlTask = new CurlTask("http://s-analytics-ci.patsnap.local:8081/analytics/1.0/feedback?feedback_type=new_search_ui");
 
     @Test
     public void testTaskTimes() throws Exception {
-        long exhaust = latchDemo.taskTimes(5, curlTask);
+        long exhaust = latchDemo.taskTimes(1, curlTask);
         System.out.println(exhaust);
     }
 }
