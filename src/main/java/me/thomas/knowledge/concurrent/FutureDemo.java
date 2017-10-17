e98b9b4579508c7232a43d4c365794293944a6bb
Zhao Xinsheng <zhaoxinsheng@patsnap.com>
2017-10-17 22:54:33 +0800

    超时时间计时处研究

--- src/main/java/me/thomas/knowledge/concurrent/FutureDemo.java
+++ src/main/java/me/thomas/knowledge/concurrent/FutureDemo.java
@@ -2,6 +2,7 @@ package me.thomas.knowledge.concurrent;
 
 import me.thomas.knowledge.algorithm.dp.Fibonacci;
 
+import java.sql.Time;
 import java.util.Date;
 import java.util.concurrent.*;
 
@@ -17,7 +18,7 @@ public class FutureDemo {
         FutureTask<Long> futureTask = new FutureTask<>(new Callable<Long>() {
             @Override
             public Long call() throws Exception {
-                return fibonacci.fib(8);
+                return fibonacci.fib(44);
             }
         });
         // 一定要在新线程中启动这个任务
@@ -28,21 +29,30 @@ public class FutureDemo {
         Future<Long> future = executor.submit(new Callable<Long>() {
             @Override
             public Long call() throws Exception {
-                return fibonacci.fib(48);
+                long startTime = System.nanoTime();
+                long value = fibonacci.fib(44);
+                System.out.println("Spent time: " + (System.nanoTime() - startTime));
+                return value;
             }
         });
         // 只接受一个任务，做完任务后需要shutdown
         executor.shutdown();
 
         try {
+            System.out.println("Begin time: \t\t\t" + System.currentTimeMillis());
+            TimeUnit.SECONDS.sleep(2); // sleep(2s)后futureTask.get(3s)不超时，否则超时。
+            System.out.println("After sleep time: \t\t" + System.currentTimeMillis());
             // future.get()方法是会阻塞当前线程的操作
-            System.out.println("newFutureTask: " + futureTask.get() + " at " + new Date().getTime());
+            System.out.println("newFutureTask: " + futureTask.get(3, TimeUnit.SECONDS) + " at " + new Date().getTime());
             System.out.println("executor.submit: " + future.get() + " at " + new Date().getTime());
             System.out.println("over");
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         } catch (ExecutionException e) {
             e.printStackTrace();
+        } catch (TimeoutException e) {
+            System.out.println("TimeoutException time: " + System.currentTimeMillis());
+            futureTask.cancel(true);
         }
     }
 }
