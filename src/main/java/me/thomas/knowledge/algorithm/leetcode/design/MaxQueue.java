4dec9daa37d18f8fa6ad1791402935dcebdbdca7
Zhao Xinsheng <xinsheng2.zhao@ly.com>
2022-07-03 16:13:14 +0800

    monotonic queue, accept.

---
+++ src/main/java/me/thomas/knowledge/algorithm/leetcode/design/MaxQueue.java
@@ -0,0 +1,42 @@
+package me.thomas.knowledge.algorithm.leetcode.design;
+
+import java.util.*;
+
+/**
+ * 请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
+ * <p>
+ * 若队列为空，pop_front 和 max_value需要返回 -1
+ *
+ * @author xinsheng2.zhao
+ * @version Id: MaxQueue.java, v 0.1 2022/7/3 15:49 xinsheng2.zhao Exp $
+ */
+public class MaxQueue {
+
+    private final Queue<Integer> queue;
+    private final Deque<Integer> deque;
+
+    public MaxQueue() {
+        queue = new ArrayDeque<>();
+        deque = new LinkedList<>();
+    }
+
+    public int max_value() {
+        return deque.isEmpty() ? -1 : deque.peekFirst();
+    }
+
+    public void push_back(int value) {
+        queue.offer(value);
+        while (!deque.isEmpty() && deque.peekLast() < value) {
+            deque.pollLast();
+        }
+        deque.offerLast(value);
+    }
+
+    public int pop_front() {
+        int value = queue.isEmpty() ? -1 : queue.poll();
+        if (value > 0 && !deque.isEmpty() && deque.peekFirst() == value) {
+            deque.pollFirst();
+        }
+        return value;
+    }
+}
