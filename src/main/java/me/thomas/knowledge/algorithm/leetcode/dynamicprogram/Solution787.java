598a2865db232c35f9d9c79cfa9e59da3367774a
Zhao Xinsheng <xinsheng2.zhao@ly.com>
2022-07-17 21:22:58 +0800

    a*b mod k == (a mod k) * (b mod k) mod k, accept.

---
+++ src/main/java/me/thomas/knowledge/algorithm/leetcode/dynamicprogram/Solution787.java
@@ -0,0 +1,18 @@
+package me.thomas.knowledge.algorithm.leetcode.dynamicprogram;
+
+import java.util.*;
+
+/**
+ * 有 n 个城市通过一些航班连接。给你一个数组flights ，其中flights[i] = [fromi, toi, pricei] ，表示该航班都从城市 fromi 开始，以价格 pricei 抵达 toi。
+ * <p>
+ * 现在给定所有的城市和航班，以及出发城市 src 和目的地 dst，你的任务是找到出一条最多经过 k站中转的路线，使得从 src 到 dst 的 价格最便宜 ，并返回该价格。 如果不存在这样的路线，则输出 -1。
+ *
+ * @author xinsheng2.zhao
+ * @version Id: Solution787.java, v 0.1 2022/6/25 13:50 xinsheng2.zhao Exp $
+ */
+public class Solution787 {
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        return 0;
+    }
+}
