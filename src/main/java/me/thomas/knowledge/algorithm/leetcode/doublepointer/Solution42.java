85d4550ce00271bbc83c55374adc200d04842370
Zhao Xinsheng <xinsheng2.zhao@ly.com>
2022-07-27 21:42:38 +0800

    doubble pointer, coding after understand the resolve method, accept.

--- src/main/java/me/thomas/knowledge/algorithm/leetcode/doublepointer/Solution42.java
+++ src/main/java/me/thomas/knowledge/algorithm/leetcode/doublepointer/Solution42.java
@@ -10,44 +10,33 @@ package me.thomas.knowledge.algorithm.leetcode.doublepointer;
 public class Solution42 {
 
     /**
+     * 思路：每个单位宽度能装的水的容量由其两侧最高的柱子中较矮的那根决定。
      * 输入：height = [4,2,0,3,2,5]
      * 输出：9
      */
     public int trap(int[] height) {
         int answer = 0;
-        int left = 0, right = 0;
-        // left最多只能寻找到倒数第二个槽位
-        while (left < height.length - 1) {
-            // 找到下一根柱子
-            if (height[left] > 0) {
-                right = left + 1;
-                int maxHeightIndex = right;
-                // 向右找到第一根比height[left]高的柱子，不存在的情况下找到右侧最高的柱子
-                while (right < height.length) {
-                    if (height[maxHeightIndex] < height[right]) {
-                        maxHeightIndex = right;
-                    }
-                    if (height[left] < height[right]) {
-                        break;
-                    }
-                    right++;
-                }
-                right = maxHeightIndex;
-                // 计算[left, right]区间内存储的雨水量
-                int lowWaterLine = Math.min(height[left], height[right]);
-                for (int i = left + 1; i < right; i++) {
-                    answer += lowWaterLine - height[i];
-                }
-                left = right - 1;
+        int maxLeftHeight = 0, maxRightHeight = 0; /* 左右两侧的最高的柱子的高度 */
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            // 哪边柱子矮，哪边就前进一步。
+            int leftHeight = height[left], rightHeight = height[right];
+            maxLeftHeight = Math.max(maxLeftHeight, leftHeight);
+            maxRightHeight = Math.max(maxRightHeight, rightHeight);
+            if (leftHeight < rightHeight) {
+                answer += maxLeftHeight - leftHeight;
+                left++;
+            } else {
+                answer += maxRightHeight - rightHeight;
+                right--;
             }
-            left++;
         }
         return answer;
     }
 
     public static void main(String[] args) {
         Solution42 solution = new Solution42();
-        System.out.println(solution.trap(new int[] { 4, 2, 0, 3, 2, 5 }));
-        System.out.println(solution.trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
+        System.out.println(solution.trap(new int[] { 4, 2, 0, 3, 2, 5 })); // 9
+        System.out.println(solution.trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 })); // 6
     }
 }
