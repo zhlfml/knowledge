e849c92e66b9c51ff63e0d7f7b67aaa116e1a6a0
Zhao Xinsheng <xinsheng2.zhao@ly.com>
2022-08-07 21:28:38 +0800

    np, solution 2, accept.

--- src/main/java/me/thomas/knowledge/algorithm/leetcode/dynamicprogram/Solution1312.java
+++ src/main/java/me/thomas/knowledge/algorithm/leetcode/dynamicprogram/Solution1312.java
@@ -14,54 +14,28 @@ package me.thomas.knowledge.algorithm.leetcode.dynamicprogram;
 public class Solution1312 {
 
     /**
-     * 思路：先求出最长回文子序列，再将总长度减去最长回文子序列的长度，结果就是需要插入的字符数。
+     * 思路：动态规划直接求出最少操作次数。
      */
     public int minInsertions(String s) {
         if (s == null) {
             return 0;
         }
-        return s.length() - longestPalindromeSubSeq(s);
-    }
-
-    /**
-     * 输入：s = "bbbab"
-     * 输出：4
-     * 解释：一个可能的最长回文子序列为 "bbbb" 。
-     */
-    int longestPalindromeSubSeq(String s) {
-        if (s == null) {
-            return 0;
-        }
         int len = s.length();
         if (len < 2) {
-            return len;
+            return 0;
         }
         char[] str = s.toCharArray();
-        int[][] dp = new int[len][len]; /* dp含义：字符串s从i到j的最大回文子序列的长度为dp[i][j] */
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = 1;
-        }
-        /*
-         * 从下向上从左到右遍历
-         * 计算最长回文子序列时分为两种情况
-         *  1. i和j所在的字符不等，那么dp[i][j]的最大回文子序列就是Math.max(dp[i + 1][j], dp[i][j - 1])
-         *  2. i和j所在的字符相等, 又细分为两种情况
-         *  2.1 i和j相邻，那么dp[i][j]==2
-         *  2.2 i和j不相邻，那么dp[i][j]==2+dp[i + 1][j - 1]
-         */
+        int[][] dp = new int[len][len]; /* dp含义：字符串s从i到j成为回文子串的最少操作次数 */
         for (int i = len - 1; i >= 0; i--) {
             for (int j = i + 1; j < len; j++) {
                 if (str[i] != str[j]) {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-                } else {
-                    if (j - i == 1) {
-                        dp[i][j] = 2;
-                    } else {
-                        dp[i][j] = 2 + dp[i + 1][j - 1];
-                    }
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                } else if (j - i > 1) { /* 其实无需判断`j - i > 1` */
+                    dp[i][j] = dp[i + 1][j - 1];
                 }
             }
         }
         return dp[0][len - 1];
     }
+
 }
