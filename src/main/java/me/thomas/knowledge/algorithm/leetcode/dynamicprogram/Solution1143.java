c5572845543bb0e4fc647a86e82d74ba8ad9f1ad
Zhao Xinsheng <xinsheng2.zhao@ly.com>
2022-08-07 15:19:24 +0800

    labuladong, accept.

---
+++ src/main/java/me/thomas/knowledge/algorithm/leetcode/dynamicprogram/Solution1143.java
@@ -0,0 +1,46 @@
+package me.thomas.knowledge.algorithm.leetcode.dynamicprogram;
+
+/**
+ * 1143. 最长公共子序列
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ * <p>
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * <p>
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ *
+ * @author xinsheng2.zhao
+ * @version Id: Solution1143.java, v 0.1 2022/8/7 15:07 xinsheng2.zhao Exp $
+ */
+public class Solution1143 {
+
+    /**
+     * 输入：text1 = "abcde", text2 = "ace"
+     * 输出：3
+     * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        if (text1 == null || text1.length() == 0) {
+            return 0;
+        }
+        if (text2 == null || text2.length() == 0) {
+            return 0;
+        }
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1]; /* dp含义：长度为m和n的两个字符串的最长公共子序列长度 */
+        /*
+         * 显然，其中一个字符串长度为0时，最长公共子序列必然为0，所以我们从长度为1的字符串开始推导
+         */
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+                // System.out.printf("dp[%d][%d] = %d\n", i, j, dp[i][j]);
+            }
+        }
+        return dp[m][n];
+    }
+}
