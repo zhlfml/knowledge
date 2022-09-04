25bc4f505ba4a08db13de2f6a32422c94c2ea092
Zhao Xinsheng <xinsheng2.zhao@ly.com>
2022-09-04 20:05:19 +0800

    dp, np, accept.

--- src/main/java/me/thomas/knowledge/algorithm/leetcode/dynamicprogram/game/sock/Solution122.java
+++ src/main/java/me/thomas/knowledge/algorithm/leetcode/dynamicprogram/game/sock/Solution122.java
@@ -43,4 +43,19 @@ public class Solution122 {
         }
         return answer;
     }
+
+    /**
+     * 思路：动态规划
+     * 每天都买卖一次，如果是正收益就算入总额，负收益则当做没有此次交易。
+     */
+    int maxProfit_dp(int[] prices) {
+        int[][] dp = new int[prices.length][2]; /* 含义见Solution121 */
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[prices.length - 1][0];
+    }
 }
