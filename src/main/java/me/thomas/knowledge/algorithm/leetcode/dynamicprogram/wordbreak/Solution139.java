95d87395af26f17c31cc83a9f817c994cc2f420b
Zhao Xinsheng <xinsheng2.zhao@ly.com>
2022-10-24 23:28:38 +0800

    dp, improve performance, written myself, accept.

--- src/main/java/me/thomas/knowledge/algorithm/leetcode/dynamicprogram/wordbreak/Solution139.java
+++ src/main/java/me/thomas/knowledge/algorithm/leetcode/dynamicprogram/wordbreak/Solution139.java
@@ -1,6 +1,8 @@
 package me.thomas.knowledge.algorithm.leetcode.dynamicprogram.wordbreak;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 139. 单词拆分
@@ -20,37 +22,33 @@ public class Solution139 {
      * 注意，你可以重复使用字典中的单词。
      * <p>
      * 思路：有网友说是背包型动态规划，但我看像区间型动态规划。
-     * 递推关系式：若dp[i][j]可以被拼出且dp[j][k]可以被拼出，那么dp[i][k]也可以被拼出
+     * 递推关系式：若dp[i][j]可以被拼出且dp[j][k]可以被拼出，那么dp[i][k]也可以被拼出。
+     * 得知这是动态规划题之后，发现此题和gameOfFivePowers（由C语言实现）非常类似，所以可以参考那道题的思路。
      */
     public boolean wordBreak(String s, List<String> wordDict) {
         if (s == null || s.length() == 0) {
             return true;
         }
+        Set<String> wordDictSet = new HashSet<>(wordDict);
         int len = s.length();
-        boolean[][] dp = new boolean[len + 1][len + 1]; /* 含义：字符串s的区间[i, j]是否可以利用字典中出现的单词拼接出 */
-        // base case：0字符长度都为true
-        for (int i = 0; i <= len; i++) {
-            dp[i][i] = true;
-        }
-        for (int i = len - 1; i >= 0; i--) {
-            for (int j = i + 1; j <= len; j++) {
-                for (int k = j; k > i; k--) {
-                    dp[i][j] = dp[k][j] && isWordInDict(s.substring(i, k), wordDict);
-                    if (dp[i][j]) {
+        boolean[] dp = new boolean[len + 1]; /* 含义：字符串s的区间从0到i是否可以利用字典中出现的单词拼接出记为dp[i] */
+        // base case：0字符长度为true
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            if (wordDictSet.contains(s.substring(0, i))) {
+                dp[i] = true;
+            }
+            // s[0..i]不是一个完整的单词，那么尝试分解，查看其是否为二个或以上单词组合而成。
+            else {
+                for (int j = i - 1; j >= 1; j--) { /* 倒过来判断性能更高 */
+                    dp[i] = dp[j] && wordDictSet.contains(s.substring(j, i));
+                    if (dp[i]) {
                         break;
                     }
                 }
             }
         }
-        return dp[0][len];
+        return dp[len];
     }
 
-    boolean isWordInDict(String s, List<String> wordDict) {
-        for (String dict : wordDict) {
-            if (dict.equals(s)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
